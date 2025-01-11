a6958e5a153dafe354f80f17d1e0158c1b2cca5a
JakkuSakura <qjk2001@gmail.com>
2025-01-11 23:20:05 +0800

    feat: animation-cpu example

--- README.md
+++ README.md
@@ -12,10 +12,29 @@ Due to gpui not being published on crates.io, you need to add the following to y
 plotters-gpui = { git = "https://github.com/JakkuSakura/plotters-gpui" }
 ```
 
-You might be interested in [https://github.com/JakkuSakura/gpui-plot](https://github.com/JakkuSakura/gpui-plot), as it provides interactivity and more stuff on top of plotters-gpui
+You might be interested in [https://github.com/JakkuSakura/gpui-plot](https://github.com/JakkuSakura/gpui-plot), as it
+provides interactivity and more stuff on top of plotters-gpui
 
 ## Show cases
 
 <img width="300" alt="image" src="https://github.com/user-attachments/assets/276b75c2-d5fe-4b0e-93b1-1215317d4b73" /> <img width="300" alt="image" src="https://github.com/user-attachments/assets/8b1f7c80-ef09-4ffd-aff8-123315ecf1b3" />
 <img width="300" alt="image" src="https://github.com/user-attachments/assets/7e9ec94e-a8f0-4e0d-97eb-9399f0145e39" /> <img width="300" alt="image" src="https://github.com/user-attachments/assets/03ea4351-d079-4372-af84-bd2429ccc098" />
 <img width="300" alt="image" src="https://github.com/user-attachments/assets/56c29590-c120-4a5e-8b65-b272afe732dc" />
+
+## Performance
+
+plotters-gpui try to maintain very good performance compared to that in javascript/python, even better than CPU based
+plotting libraries
+
+### Metal Performance HUD
+
+Use the following environment variable to enable Metal Performance HUD:
+
+```text
+MTL_HUD_ENABLED=1
+Enables the Metal Performance HUD.
+```
+
+```shell
+MTL_HUD_ENABLED=1 cargo run ...
+```
---
+++ examples/animation-cpu.rs
@@ -0,0 +1,139 @@
+use gpui::{div, prelude::*, App, AppContext, View, ViewContext, WindowContext, WindowOptions};
+use parking_lot::RwLock;
+use plotters::coord::Shift;
+use plotters::drawing::DrawingArea;
+use plotters::prelude::*;
+use plotters_gpui::backend::GpuiBackend;
+use plotters_gpui::element::{PlottersChart, PlottersDrawAreaModel, PlottersDrawAreaViewer};
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::time::SystemTime;
+use sysinfo::{Pid, ProcessesToUpdate, System};
+
+struct MainViewer {
+    figure: View<PlottersDrawAreaViewer>,
+    animation: bool,
+}
+
+impl MainViewer {
+    fn new(model: Rc<RwLock<PlottersDrawAreaModel>>, cx: &mut WindowContext) -> Self {
+        let figure = PlottersDrawAreaViewer::with_shared_model(model);
+
+        Self {
+            figure: cx.new_view(move |_| figure),
+            animation: false,
+        }
+    }
+}
+
+impl Render for MainViewer {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        cx.defer(move |this, cx| {
+            if this.animation {
+                cx.notify();
+            }
+        });
+        div()
+            .size_full()
+            .flex_col()
+            .bg(gpui::white())
+            .text_color(gpui::black())
+            .child(self.figure.clone())
+    }
+}
+struct CpuUsage {
+    system: System,
+    pid: Pid,
+    points: VecDeque<(SystemTime, f32)>,
+}
+impl CpuUsage {
+    fn new() -> Self {
+        Self {
+            system: System::new_all(),
+            pid: (std::process::id() as usize).into(),
+            points: VecDeque::new(),
+        }
+    }
+    fn should_sample(&self) -> bool {
+        if self.points.is_empty() {
+            return true;
+        }
+        let (time, _) = self.points.back().unwrap();
+        time.elapsed().unwrap().as_secs_f32() > 0.1
+    }
+    fn try_sample(&mut self) {
+        if !self.should_sample() {
+            return;
+        }
+        self.system.refresh_processes(ProcessesToUpdate::All, false);
+        let process = self.system.process(self.pid).unwrap();
+        self.points
+            .push_back((SystemTime::now(), process.cpu_usage()));
+        if self.points.len() > 100 {
+            self.points.pop_front();
+        }
+    }
+    fn get_line(&mut self) -> Vec<(f32, f32)> {
+        let mut line = Vec::new();
+        let time = self.points.front().unwrap().0;
+        for (x, y) in self.points.iter() {
+            let seconds = x.duration_since(time).unwrap().as_secs_f32();
+            line.push((seconds, *y));
+        }
+        line
+    }
+}
+
+impl PlottersChart for CpuUsage {
+    fn plot(
+        &mut self,
+        root: &DrawingArea<GpuiBackend, Shift>,
+    ) -> Result<(), plotters_gpui::DrawingErrorKind> {
+        self.try_sample();
+        let line = self.get_line();
+        let x_min = line.first().map(|(x, _)| *x).unwrap_or(0.0);
+        let x_max = line.last().map(|(x, _)| *x).unwrap_or(1.0);
+        let y_min = line.iter().map(|(_, y)| *y).fold(f32::INFINITY, f32::min);
+        let y_max = line
+            .iter()
+            .map(|(_, y)| *y)
+            .fold(f32::NEG_INFINITY, f32::max);
+        let mut chart = ChartBuilder::on(root)
+            .caption("CPU Usage", ("sans-serif", 24).into_font())
+            .margin(5)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(x_min..x_max, y_min..y_max)
+            .unwrap();
+        chart.configure_mesh().draw().unwrap();
+        println!("Plot line with {} points", line.len());
+        chart.draw_series(LineSeries::new(line, &BLACK)).unwrap();
+
+        Ok(())
+    }
+}
+
+fn main_viewer(cx: &mut WindowContext) -> MainViewer {
+    let figure = PlottersDrawAreaModel::new(Box::new(CpuUsage::new()));
+    let mut main_viewer = MainViewer::new(Rc::new(RwLock::new(figure)), cx);
+    main_viewer.animation = true;
+
+    main_viewer
+}
+
+fn main() {
+    App::new().run(move |cx: &mut AppContext| {
+        cx.open_window(
+            WindowOptions {
+                focus: true,
+                ..Default::default()
+            },
+            move |cx| {
+                let view = main_viewer(cx);
+                cx.new_view(move |_| view)
+            },
+        )
+        .unwrap();
+        cx.activate(true);
+    });
+}
