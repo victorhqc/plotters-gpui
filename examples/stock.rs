be6171caeb6f90914f4498ecd286181460c9ec52
JakkuSakura <qjk2001@gmail.com>
2025-03-23 00:44:57 +0800

    fix: fix rust versions

--- examples/stock.rs
+++ examples/stock.rs
@@ -1,5 +1,8 @@
 use chrono::Duration;
-use gpui::{div, prelude::*, App, AppContext, Application, Context, Entity, Window, WindowOptions};
+use gpui::{
+    div, prelude::*, px, size, App, AppContext, Application, Bounds, Context, Entity, Window,
+    WindowBounds, WindowOptions,
+};
 use parking_lot::RwLock;
 use plotters::coord::Shift;
 use plotters::prelude::*;
@@ -124,15 +127,14 @@ fn main_viewer(cx: &mut App) -> MainViewer {
 
 fn main() {
     Application::new().run(move |cx: &mut App| {
+        let bounds = Bounds::centered(None, size(px(800.0), px(600.0)), cx);
+
         cx.open_window(
             WindowOptions {
-                focus: true,
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
                 ..Default::default()
             },
-            move |_, cx| {
-                let view = main_viewer(cx);
-                cx.new(move |_| view)
-            },
+            move |_, cx| cx.new(move |cx| main_viewer(cx)),
         )
         .unwrap();
         cx.activate(true);
